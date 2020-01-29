75b4abe10ecf8efd8843795a1203c90edea4fcfd
SYED EBAD <syed.ebad1999@gmail.com>
2020-01-29 22:59:35 +0500

    Data Fatched

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var xlsxtojson = require("xlsx-to-json");
+var xlstojson = require("xls-to-json");
 
 //Init app
 const app = express();
@@ -10,8 +12,34 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
+// app.use(function(req, res, next) { //allow cross origin requests
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Methods", "POST, PUT, OPTIONS, DELETE, GET");
+//     res.header("Access-Control-Max-Age", "3600");
+//     res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+//     next();
+// });
+
+// // configuration
+// app.use(express.static(__dirname + '/public'));                
+// app.use('/public/uploads',express.static(__dirname + '/public/uploads'));  
+
 app.get('/',(req,res)=>{
     res.status(200).send('Hello World');
 })
+
+app.post('/api/xlstojson', function(req, res) {
+	xlsxtojson({
+		input: "./excel-to-json.xlsx",  // input xls 
+	    output: "output.json", // output json 
+	    lowerCaseHeaders:true
+	}, function(err, result) {
+	    if(err) {
+	      res.json(err);
+	    } else {
+	      res.json(result);
+	    }
+	});
+});
 
 app.listen(5000, () => console.log(`Node Server running !`));
